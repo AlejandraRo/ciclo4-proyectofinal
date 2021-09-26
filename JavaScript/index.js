a999e66f07ed1261ff39c7480fedd2795db0f6f8
Brandom Mosquera <bcmc-97@hotmailo.com>
2021-09-26 12:40:11 -0500

    Projects model and controller added, Sprint 3 docx updated

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const userRoutes = require('./src/controllers/user.controller');
-const proyectRoutes = require('./src/controllers/proyect.controller');
+const userRoutes = require("./src/controllers/user.controller");
+const proyectRoutes = require("./src/controllers/proyect.controller");
 
 const app = express();
 
@@ -17,21 +17,21 @@ app.use(express.json());
 //Connection to mongodb Atlas with mongoose
 mongoose.connect(DB_ATLAS, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
-const connection=mongoose.connection
-connection.once('open', () => console.log('DB Connected'));
+const connection = mongoose.connection;
+connection.once("open", () => console.log("DB Connected"));
 
 //Route path on proyect
-app.get('/', (req, res) => {
-  res.send('Proyect Managment App Mintic running');
+app.get("/", (req, res) => {
+  res.send("Proyect Managment App Mintic running");
 });
 
 //Routes path on users
-app.use('/users', userRoutes);
+app.use("/users", userRoutes);
 
 //Routes path on proyects
-// app.use('/proyects', proyectRoutes); ----------> //DESCOMENTAR CUANDO SE CREEN LOS CONTROLLERS Y LOS MODELS
+app.use("/proyects", proyectRoutes);
 
 app.listen(PORT, () => {
   console.log(`app listening at port ${PORT}`);
