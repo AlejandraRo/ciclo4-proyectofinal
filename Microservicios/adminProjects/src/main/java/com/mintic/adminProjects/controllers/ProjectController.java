3866eafa9c49f7bdc021577581aaa0f2f7049b31
marrsd <mpenaranda1@misena.edu.co>
2021-10-07 10:25:57 -0500

    get particular project by name

--- Microservicios/adminProjects/src/main/java/com/mintic/adminProjects/controllers/ProjectController.java
+++ Microservicios/adminProjects/src/main/java/com/mintic/adminProjects/controllers/ProjectController.java
@@ -1,5 +1,25 @@
 package com.mintic.adminProjects.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mintic.adminProjects.entities.ProjectEntity;
+import com.mintic.adminProjects.repositories.ProjectRespository;
+
+@RestController
 public class ProjectController {
+
+  private final ProjectRespository projectRepository;
+	
+	public ProjectController(ProjectRespository projectRepository) {
+		this.projectRepository = projectRepository;
+	}
+	
+	//GET PROJECT BY NAME
+	@GetMapping("/proyectos/{name}")
+	public ProjectEntity getProjectByName(@PathVariable String name) {
+		return projectRepository.findProjectByName(name);
+	}
 
 }
--- Microservicios/adminProjects/src/main/java/com/mintic/adminProjects/repositories/ProjectRespository.java
+++ Microservicios/adminProjects/src/main/java/com/mintic/adminProjects/repositories/ProjectRespository.java
@@ -1,5 +1,12 @@
 package com.mintic.adminProjects.repositories;
 
-public interface ProjectRespository {
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.mintic.adminProjects.entities.ProjectEntity;
+
+public interface ProjectRespository extends MongoRepository<ProjectEntity, ObjectId>{
+	
+	public ProjectEntity findProjectByName(String name);
 
 }
