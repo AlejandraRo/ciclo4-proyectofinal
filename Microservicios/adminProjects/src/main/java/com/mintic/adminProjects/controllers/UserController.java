8ee89e75ed665643938b98338a3a10214bf208cd
alejandra <lejanamar@gmail.com>
2021-10-08 13:35:25 -0500

    Subiendo microservicios con creacion de usuarios

---
+++ Microservicios/adminProjects/src/main/java/com/mintic/adminProjects/controllers/UserController.java
@@ -0,0 +1,34 @@
+package com.mintic.adminProjects.controllers;
+
+import com.mintic.adminProjects.models.UserEntity;
+import com.mintic.adminProjects.repositories.ProjectRepository;
+import com.mintic.adminProjects.repositories.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+
+        UserEntity newUser01=new UserEntity("006","Scrum master","Daniel Cadavid","Ingenieria electronica","3201245678",null,null,null,"daniel.cadavid","123");
+        UserEntity newUser02=new UserEntity("008","Director","Paula Sandoval","Ingenieria de software","3201232221",null,null,null,"pau.san","abc");
+
+        this.userRepository.save(newUser01);
+        this.userRepository.save(newUser02);
+
+
+
+    }
+    @GetMapping("/UserEntity/{userId}")
+    Optional<UserEntity> getUsers(@PathVariable String userId){
+        return UserRepository.findByuserId(userId);
+    }
+
+}
---
+++ Microservicios/adminProjects/src/main/java/com/mintic/adminProjects/repositories/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.mintic.adminProjects.repositories;
+
+import com.mintic.adminProjects.models.ProjectEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ProjectRepository extends MongoRepository<ProjectEntity,String> {
+    List<ProjectEntity> findByuserId <userId> ProjectRepository(userId);
+}
